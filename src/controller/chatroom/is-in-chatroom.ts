dd76ad145afcc8270aebb0394c3919bc162e58dc
imrinzzzz <thanirin@hotmail.com>
2021-05-12 17:39:17 +0700

    edited isInChatroom

--- src/controller/chatroom/is-in-chatroom.ts
+++ src/controller/chatroom/is-in-chatroom.ts
@@ -1,10 +1,19 @@
-import { InternalServerError } from './../../middleware/500-error';
-export const isInChatroom = async (uid: string) => {
+import { InternalServerError } from "./../../middleware/500-error";
+export const isInChatroom = async (uid: string, chatroomId: number) => {
   try {
     // using ORM to interact with database
     const user = await orm.chatRoom.find({
       where: {
-        id: uid,
+        OR: [
+          {
+            id: chatroomId,
+            sellerID: uid,
+          },
+          {
+            id: chatroomId,
+            buyerID: uid,
+          },
+        ],
       },
     });
     return true ? user !== null : false;
