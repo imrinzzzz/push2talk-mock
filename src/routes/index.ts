7a67fba9e4e82d8843d21f169da09eb24b16cfa4
imrinzzzz <thanirin@hotmail.com>
2021-05-12 18:28:23 +0700

    added route to push2talk routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,9 @@
 import express from "express";
 
+import Push2TalkRouter from "./push-to-talk";
+
 const router = express.Router();
+
+router.use("/push2talk", Push2TalkRouter);
 
 export const Router = router;
